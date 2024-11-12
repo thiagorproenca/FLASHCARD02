f2a6ec1a2a933ec60b360584a1e94c4c211194ec
thiagorproenca <143522346+thiagorproenca@users.noreply.github.com>
2024-11-12 11:09:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'PREMIAÇÕES',
+    'Quantos Grammys tem Taylor Swift?',
+    '14 sendo deles 4 AYOT'
+)
+
+criaCartao(
+    'PETS',
+    'Quantos gatos Taylor Swift tem?',
+    '3'
+)
+
+criaCartao(
+    'BIOGRAFIA',
+    'Quando nasceu Taylor Swift?',
+    '13 DE DEZEMBRO DE 1989'
+)
+
+criaCartao(
+    'FATOS',
+    'Qual a maior artista da atualidade?',
+    'TAYLOR SWIFT'
+)
